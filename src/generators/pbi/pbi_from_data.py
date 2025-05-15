80d71f4b5428985dfb300510e858e70bd1623a84
Prithiraj Sengupta <prithirajsengupta@gmail.com>
2025-05-15 18:43:41 +0530

    updating pbi conversion

--- src/generators/pbi/pbi_from_data.py
+++ src/generators/pbi/pbi_from_data.py
@@ -2,7 +2,7 @@ from typing import Any, Dict
 from pybars import Compiler
 import json
 import yaml
-
+import os
 
 class PbiFromData:
 	def __init__(self):
@@ -55,11 +55,39 @@ class PbiFromData:
 	@staticmethod
 	def __get_sample_json_content(key):
 		"""
-		Read the sample JSON file and return its content as a dictionary.
-		:param key: The key to look up in the sample JSON.
-		:return: The content associated with the given key.
+		Read the sample JSON file for the given key and return its content as a dictionary.
+		:param key: The key to look up in the sample JSON mapping.
+		:return: The content of the sample JSON file.
 		"""
-		with open('src/generators/pbi/data_template_mapping.json', 'r') as file:
-			data = json.load(file)
-			return data.get(key, {})
 		
+		with open('src/generators/pbi/data_template_mapping.json', 'r') as file:
+			mapping = json.load(file)
+		entry = mapping.get(key, {})
+		data_file = entry.get('data_file')
+		if data_file and os.path.exists(data_file):
+			with open(data_file, 'r') as f:
+				return json.load(f)
+		return {}
+
+	@staticmethod
+	def __get_template_content(key: str, yaml_map: str) -> str:
+		"""
+		Read the content of a template file.
+		:param template_path: Path to the template file.
+		:return: Content of the template file as a string.
+		"""
+		template_path = f'templates/{yaml_map[key].get('template', '')}'
+		if os.path.exists(template_path):
+			with open(template_path, 'r') as file:
+				return file.read()
+		return ''
+	
+	@staticmethod
+	def __write_output_file(output_path: str, content: str) -> None:
+		"""
+		Write the rendered content to the specified output file.
+		:param output_path: Path to the output file.
+		:param content: Content to write to the output file.
+		"""
+		with open(output_path, 'w') as file:
+			file.write(content)
