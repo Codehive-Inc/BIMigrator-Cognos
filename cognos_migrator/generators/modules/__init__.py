19c4a4e796fe4372b900a4694edece9580d18d76
Rohit Rahman <rohitrhmn208@gmail.com>
2025-06-19 10:05:07 +0530

    fix: Fixed imports in `__init__` for generators/modules

--- cognos_migrator/generators/modules/__init__.py
+++ cognos_migrator/generators/modules/__init__.py
@@ -2,7 +2,7 @@
 Module-specific generators for Cognos to Power BI migration
 """
 
-from .model_file_generator import ModuleModelFileGenerator
+from .module_model_file_generator import ModuleModelFileGenerator
 from .documentation_generator import ModuleDocumentationGenerator
 
 __all__ = ['ModuleModelFileGenerator', 'ModuleDocumentationGenerator']
